01cee688bfed4040ddba0b001c6e3cd7cf2b511e
Yodel-oh-ee-dee <42037023+Cheg-ya@users.noreply.github.com>
2018-12-21 12:31:57 +0900

    fix UI, add URL, algorithm modification

--- app/index.js
+++ app/index.js
@@ -6,31 +6,73 @@ import 'styles/index.less';
 // ================================
 
 // Importing component templates
-import listTemplate from 'listView.ejs';
-import cardTemplate from 'cardView.ejs';
+import wrapperTemplate from 'wrapper.ejs';
 import headerTemplate from 'header.ejs';
 import sectionTemplate from 'section.ejs';
 import footerTemplate from 'footer.ejs';
-import sectionBody from 'sectionParent.ejs';
 
 // Import Gorilla Module
-import Gorilla from '../Gorilla';
+import Gorilla from '../Gorilla';;
 
-const footer = new Gorilla.Component(footerTemplate);
-const section = new Gorilla.Component(sectionTemplate);
+let storage = { items: [], view: "list",};
+let section = new Gorilla.Component(sectionTemplate, storage);
 const header = new Gorilla.Component(headerTemplate);
+const footer = new Gorilla.Component(footerTemplate);
+const wrapper = new Gorilla.Component(wrapperTemplate, {}, {
+  header,
+  section,
+  footer
+});
 
-Gorilla.renderToDOM(header, document.querySelector('#wrapper'));
-Gorilla.renderToDOM(section, document.querySelector('#wrapper'));
-Gorilla.renderToDOM(footer, document.querySelector('#wrapper'));
+Gorilla.renderToDOM(wrapper, document.querySelector('body'));
 
-const searchBox = document.getElementsByClassName('searchBox')[0];
+const searchBox = document.getElementById('searchBox');
+const glass = document.getElementsByClassName('glass')[0];
 const bookContainer = document.getElementById('sectionContainer');
 const listView = document.getElementById('list-view');
 const cardView = document.getElementById('card-view');
+const more = document.getElementById('button');
+
+let text;
+let displayNumber = 10;
+let urlIdx = 0;
+
+listView.addEventListener('click', function () {
+  if (!storage.items.length) return;
+  listViewer(storage.items);
+});
+
+cardView.addEventListener('click', function () {
+  if (!storage.items.length) return;
+  cardViewer(storage.items);
+});
+
+more.addEventListener('click', function () {
+  displayNumber+= 10;
+  
+  if (displayNumber > 100) {
+    alert("검색 결과는 최대 100개 이하 입니다.");
+    throw new Error('검색 결과 한도 초과');
+  }
+
+  searchAjax(text);
+});
+
+glass.addEventListener('click', function (ev) {
+  displayNumber = 10;
+  ev.preventDefault();
+
+  if (searchBox.value.length > 20) {
+    alert('검색어는 1글자 이상 20글자 이하 (공백 포함)여야 합니다.');
+    throw new Error('글자수 제한 에러');
+  }
+
+  text = searchBox.value;
+  searchAjax(text, displayNumber);
+});
 
 searchBox.addEventListener('keyup', function (ev) {
-
+  displayNumber = 10;
   ev.preventDefault();
 
   if (ev.target.value.length > 20) {
@@ -44,20 +86,20 @@ searchBox.addEventListener('keyup', function (ev) {
       throw new Error('글자수 제한 에러');
     }
 
-    let text = ev.target.value;
-    ajax(text);
+    text = ev.target.value;
+    searchAjax(text, displayNumber);
   }
 });
 
-function ajax (input) {
+function searchAjax (input) {
   let request = new XMLHttpRequest();
-  let target = input
+  let target = input;
+  urlIdx = 0;
 
-  request.open('GET', 'http://localhost:3000/v1/search/book?query='+ target +'&display=20', true);
-  
+  request.open('GET', 'http://localhost:3000/v1/search/book?query='+ target +'&display=' + displayNumber, true);
+
   request.onreadystatechange = function () {
     if (request.readyState === 4 && request.status === 200) {
-      
       let originData = request.responseText;
       originData = JSON.parse(originData);
 
@@ -72,100 +114,92 @@ function ajax (input) {
         originData.items[i].description = originData.items[i].description.split('<b>').join('').split('</b>').join('');
         originData.items[i].pubdate = `${originData.items[i].pubdate.slice(0,4)}년 ${originData.items[i].pubdate.slice(4,6)}월 ${originData.items[i].pubdate.slice(6)}일`;
       }
-      // debugger;
-      if (!bookContainer.children.length) {
-        listViewer(originData, bookContainer);   
 
-        console.log(originData.items);
-
-        listView.addEventListener('click', function () {
-          listViewer(originData, bookContainer);
-        });
-
-        cardView.addEventListener('click', function () {
-          cardViewer(originData, bookContainer);
-        });
-      } else {
-        //기존 컴포넌트의 데이터만 바꾸기
-      }
-
+      urlAjax(originData.items)
+      
     } else if (request.readyState === 4 && request.status === 404){
       alert('서버에 자료 없음');
       throw new Error('404 Not found');
     }
   }
+
   request.send(null);
 }
 
-function cardViewer (data, container) {
-  bookContainer.innerHTML= '';
+function urlAjax (data) {
+  let items = data;
+  let urlRequest = new XMLHttpRequest();
 
-  data.items.forEach(function (v) {
-    if (v.title.includes('(')) {
-      v.title = v.title.split('(')[0].trim();
-      return v.title;
+  urlRequest.open('GET', 'http://localhost:3000/v1/util/shorturl?url=' + items[urlIdx].link, true);
+
+  urlRequest.onreadystatechange = function () {
+    if (urlRequest.readyState === 4 && urlRequest.status === 200) {
+      let url = urlRequest.responseText;
+      url = JSON.parse(url);
+
+      items[urlIdx].url = url.result.url;
+
+      if (urlIdx === displayNumber-1 && !bookContainer.children.length) {
+        if (storage.view === "list") {
+          return listViewer(data);
+        }
+        
+        return cardViewer(data);
+      }
+
+      urlIdx++;
+
+      urlAjax(items);
     }
-  })
-
-  console.log(data);
-
-  let bookCase = {};
-
-  for (let i = 0; i < 20; i++) {
-    if (data.items[i].description) {
-      data.items[i].description = data.items[i].description.slice(0, 50) + "...";
-    } else {
-      data.items[i].description = "설명 누락 정보";
-    }
-
-    bookCase["book" + (i + 1)] = new Gorilla.Component(cardTemplate, data.items[i]);
   }
 
-  let sectionParent = new Gorilla.Component(sectionBody, {}, bookCase);
-  Gorilla.renderToDOM(sectionParent, container);
+  urlRequest.send(null);
+}
 
-  for (let j = 0; j < 20; j++) {
-    let bookImage = container.getElementsByClassName('cardBookCover')[j];
-    bookImage.setAttribute('src', bookCase["book" + (j + 1)].image);
+function cardViewer (data) {
+  bookContainer.innerHTML= '';
+
+  data.forEach(function (v) {
+    if (v.title.includes('(')) {
+      v.title = v.title.split('(')[0].trim();
+    }
+  });
+  // 저자 2명이상 일때 ... 외 1명
+  for (let i = 0; i < data.length; i++) {
+    if (!data[i].description) {
+      data[i].description = "누락된 데이터";
+    }
+  }
+
+  storage["items"] = data;
+  storage["view"] = "card";
+  section.render();
+
+  if (more.classList.contains('hide')) {
+    more.classList.remove('hide');
+    more.classList.add('visibility');
   }
 }
 
-function listViewer (data, container) {
+function listViewer (data) {
   bookContainer.innerHTML= '';
 
-  let bookCase = {};
-
-  for (let i = 0; i < 20; i++) {
-    if (data.items[i].description) {
-      data.items[i].description = data.items[i].description.slice(0, 50) + "...";
-
-    } else {
-      data.items[i].description = "누락된 정보";
+  for (let i = 0; i < data.length; i++) {
+    if (!data[i].description) {
+      data[i].description = "누락된 데이터";
     }
-
-    bookCase["book" + (i + 1)] = new Gorilla.Component(listTemplate, data.items[i]);
   }
 
-  let sectionParent = new Gorilla.Component(sectionBody, {}, bookCase);
-  Gorilla.renderToDOM(sectionParent, container);
+  storage["items"] = data;
+  storage["view"] = "list";
+  section.render();
 
-  for (let j = 0; j < 20; j++) {
-    let bookImage = container.getElementsByClassName('listBookCover')[j];
-    bookImage.setAttribute('src', bookCase["book" + (j + 1)].image);
+  if (more.classList.contains('hide')) {
+    more.classList.remove('hide');
+    more.classList.add('visibility');
   }
 }
 
 /* DO NOT REMOVE */
 module.hot.accept();
 /* DO NOT REMOVE */
-
-// Listening to component life cycle
-// app.on('BEFORE_RENDER', () => console.log('app before render'));
-// app.on('AFTER_RENDER', () => console.log('app after render'));
-
-// Updating component data model //스크롤 다내렸을때 20개 다시 자동으로 불러오는 기능?
-// setTimeout(() => {
-//   app.title = '빠닐라코띵';
-//   baby.name = 'Qkqkqkqk';
-//   grandBaby.content = 'GGGGG';
-// }, 2000);
